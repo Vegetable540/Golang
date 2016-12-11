2c51603831cbb46e7991c0d0fff2c130fda5aeef
liurx <lrx91691@gmail.com>
2016-12-11 02:39:28 -0500

    完善测试代码

--- LongConnectTest/Websocket/Client/main.go
+++ LongConnectTest/Websocket/Client/main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"golang.org/x/net/websocket"
+	"log"
+)
 
+const ServerIP string = "127.0.0.1" //"192.168.222.128"
+
+func ConnectServer() bool {
+	ws, err := websocket.Dial("ws://"+ServerIP+":9999", "", "http://"+ServerIP+":9999")
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	socketLive := func() {
+		log.Println(ws.LocalAddr().String() + ws.RemoteAddr().String())
+		test := make(chan bool)
+		for {
+			t := <-test
+			log.Println(t)
+		}
+	}
+
+	go socketLive()
+	return true
+}
+
+func main() {
+	for {
+		if !ConnectServer() {
+			return
+		}
+	}
 }
--- LongConnectTest/Websocket/Server/main.go
+++ LongConnectTest/Websocket/Server/main.go
@@ -4,10 +4,25 @@ import "net/http"
 import "golang.org/x/net/websocket"
 import "log"
 
-func socket(ws *websocket.Conn) {
-	log.Print(ws.RemoteAddr)
+func socketLive(ws *websocket.Conn) {
+	testCh := make(chan bool)
+	for {
+		test := <-testCh
+		log.Println(test)
+	}
 }
 
 func main() {
-	http.ListenAndServe("9999", websocket.Handler(socket))
+	connectedCount := 0
+	onConnected := func(ws *websocket.Conn) {
+		connectedCount++
+		go socketLive(ws)
+		log.Println(connectedCount, ws.LocalAddr().String()+ws.RemoteAddr().String())
+	}
+
+	http.Handle("/", websocket.Handler(onConnected))
+	err := http.ListenAndServe(":9999", nil)
+	if err != nil {
+		log.Println(err)
+	}
 }
